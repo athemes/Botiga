a1076bccf21fd0a3f8d7dd2dd698f60198837223
Rodrigo Teixeira <maaprote@gmail.com>
2021-08-13 17:18:08 -0300

    New version 1.0.3

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'BOTIGA_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'BOTIGA_VERSION', '1.0.1' );
+	define( 'BOTIGA_VERSION', '1.0.3' );
 }
 
 if ( ! function_exists( 'botiga_setup' ) ) :
