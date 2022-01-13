a38f80680902f93a88f412446309229481b9f0ad
Rodrigo Teixeira <maaprote@gmail.com>
2022-01-13 13:58:23 -0300

    fix

--- functions.php
+++ functions.php
@@ -403,24 +403,3 @@ function botiga_get_template_part( $slug, $name = null, $args = array() ) {
 		return include( locate_template($templates) );
 	}
 }
-
-add_filter( 'woocommerce_breadcrumb_defaults', 'woo_change_breadcrumb_home_text' );
-/**
- * Change the breadcrumb home text from "Home" to "Shop".
- * @param  array $defaults The default array items.
- * @return array           Modified array
- */
-function woo_change_breadcrumb_home_text( $defaults ) {
-	$defaults['home'] = 'Shop';
-
-	return $defaults;
-}
-
-add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
-/**
- * Change the breadcrumb home link URL from / to /shop.
- * @return string New URL for Home link item.
- */
-function woo_custom_breadrumb_home_url() {
-	return '/products/';
-}
