204425b56e3a95962fdf081961b1b419ce8f9e5d
Rodrigo Teixeira <maaprote@gmail.com>
2022-09-16 16:58:24 -0300

    fix: h1 tag for site name wrong php condition

--- inc/classes/class-botiga-header.php
+++ inc/classes/class-botiga-header.php
@@ -834,7 +834,7 @@ if ( !class_exists( 'Botiga_Header' ) ) :
 			<div class="site-branding">
 				<?php
 				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
+				if ( is_front_page() || is_home() ) :
 					?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php
