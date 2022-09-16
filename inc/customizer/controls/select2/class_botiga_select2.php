31e3ad7cd365cf895899c05dc3d19083960ee42c
Rodrigo Teixeira <maaprote@gmail.com>
2022-09-16 16:43:05 -0300

    Botiga transparent header CSS won’t load

--- inc/customizer/controls/select2/class_botiga_select2.php
+++ inc/customizer/controls/select2/class_botiga_select2.php
@@ -45,6 +45,8 @@ class Botiga_Select2_Control extends WP_Customize_Control {
 		$select2_options = $this->select2_options; 
 		$multiple        = $this->multiple; ?>
 
+		<div class="customize-control-title"><?php echo esc_html( $this->label ); ?></div>
+
 		<input type="hidden" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize-control-botiga-select2" <?php $this->link(); ?> />
 
 		<select class="botiga-select2" control-name="<?php echo esc_attr( $this->id ); ?>" data-select2-options='<?php echo esc_attr( $select2_options ); ?>'<?php echo ( $multiple ) ? ' multiple' : ''; ?>>
