f13c47d0d7e71e9f2075bc9836c939a50ae46871
Rodrigo Teixeira <maaprote@gmail.com>
2022-09-22 13:48:13 -0300

    identation

--- assets/js/src/botiga-gallery.js
+++ assets/js/src/botiga-gallery.js
@@ -11,113 +11,113 @@ var botiga = botiga || {};
 
 botiga.gallery = {
 
-    init: function() {
+	init: function () {
 
-        // fix quickview gallery thumbnails in "layout 2" mode
-        jQuery(document).on('wc-product-gallery-after-init', '.woocommerce-product-gallery', function( event, gallery ) {
+		// fix quickview gallery thumbnails in "layout 2" mode
+		jQuery(document).on('wc-product-gallery-after-init', '.woocommerce-product-gallery', function (event, gallery) {
 
-            var $gallery = jQuery(gallery);
+			var $gallery = jQuery(gallery);
 
-            if ( ! $gallery.parent().is('.gallery-quickview') ) {
-                return;
-            }
+			if (!$gallery.parent().is('.gallery-quickview')) {
+				return;
+			}
 
-            wc_single_product_params.flexslider.controlNav = 'thumbnails';
+			wc_single_product_params.flexslider.controlNav = 'thumbnails';
 
-        });
+		});
 
-        jQuery(document).on('wc-product-gallery-after-init', '.woocommerce-product-gallery', function( event, gallery ) {
+		jQuery(document).on('wc-product-gallery-after-init', '.woocommerce-product-gallery', function (event, gallery) {
 
-            var $gallery = jQuery(gallery);
+			var $gallery = jQuery(gallery);
 
-            if ( ! $gallery.parent().is('.gallery-default, .gallery-vertical, .gallery-quickview') ) {
-                return;
-            }
+			if (!$gallery.parent().is('.gallery-default, .gallery-vertical, .gallery-quickview')) {
+				return;
+			}
 
-            var flexdata = $gallery.data('product_gallery');
+			var flexdata = $gallery.data('product_gallery');
 
-            if ( ! flexdata || ! flexdata.$images ) {
-                return;
-            }
+			if (!flexdata || !flexdata.$images) {
+				return;
+			}
 
-            var $flexItems  = flexdata.$images;
-            var $flexThumbs = $gallery.find('.flex-control-thumbs');
+			var $flexItems = flexdata.$images;
+			var $flexThumbs = $gallery.find('.flex-control-thumbs');
 
-            // pass carousel sliders if less than 5 items
-            if ( $flexItems.length <= 5 ) {
-                return;
-            }
+			// pass carousel sliders if less than 5 items
+			if ($flexItems.length <= 5) {
+				return;
+			}
 
-            if ( $gallery.parent().is('.gallery-vertical') ) {
+			if ($gallery.parent().is('.gallery-vertical')) {
 
-          $flexThumbs.addClass('swiper-wrapper botiga-slides');
-          $flexThumbs.find('li').addClass('swiper-slide');
-          $flexThumbs.wrapAll('<div class="swiper botiga-swiper"></div>');
+				$flexThumbs.addClass('swiper-wrapper botiga-slides');
+				$flexThumbs.find('li').addClass('swiper-slide');
+				$flexThumbs.wrapAll('<div class="swiper botiga-swiper"></div>');
 
-          var $swiper = $gallery.find('.botiga-swiper');
+				var $swiper = $gallery.find('.botiga-swiper');
 
-                $swiper.append( '<div class="botiga-swiper-button botiga-swiper-button-next"></div>' );
-                $swiper.append( '<div class="botiga-swiper-button botiga-swiper-button-prev"></div>' );
+				$swiper.append('<div class="botiga-swiper-button botiga-swiper-button-next"></div>');
+				$swiper.append('<div class="botiga-swiper-button botiga-swiper-button-prev"></div>');
 
-          var swiper = new Swiper($swiper.get(0), {
-            direction: 'vertical',
-            slidesPerView: 6,
-            spaceBetween: 20,
-            navigation: {
-              nextEl: '.botiga-swiper-button-next',
-              prevEl: '.botiga-swiper-button-prev',
-            },
-          });
+				var swiper = new Swiper($swiper.get(0), {
+					direction: 'vertical',
+					slidesPerView: 6,
+					spaceBetween: 20,
+					navigation: {
+						nextEl: '.botiga-swiper-button-next',
+						prevEl: '.botiga-swiper-button-prev',
+					},
+				});
 
-        jQuery(window).on('resize botiga.resize', function() {
+				jQuery(window).on('resize botiga.resize', function () {
 
-          var winWidth = ( window.innerWidth || document.documentElement.clientWidth );
+					var winWidth = (window.innerWidth || document.documentElement.clientWidth);
 
-         if( winWidth < 991 && swiper.params.direction !== 'horizontal' ) {
+					if (winWidth < 991 && swiper.params.direction !== 'horizontal') {
 
-            swiper.changeDirection('horizontal');
-            swiper.params.slidesPerView = 5;
-            swiper.update();
+						swiper.changeDirection('horizontal');
+						swiper.params.slidesPerView = 5;
+						swiper.update();
 
-         } else if ( winWidth > 991 && swiper.params.direction !== 'vertical' ) {
+					} else if (winWidth > 991 && swiper.params.direction !== 'vertical') {
 
-                        swiper.changeDirection('vertical');
-                        swiper.params.slidesPerView = 6;
-                        swiper.update();
+						swiper.changeDirection('vertical');
+						swiper.params.slidesPerView = 6;
+						swiper.update();
 
-         }
+					}
 
-        }).trigger('botiga.resize');
+				}).trigger('botiga.resize');
 
-            } else if ( $gallery.parent().is('.gallery-default, .gallery-quickview') ) {
+			} else if ($gallery.parent().is('.gallery-default, .gallery-quickview')) {
 
-          $flexThumbs.addClass('botiga-slides');
-          $flexThumbs.wrapAll('<div class="botiga-flexslider"></div>');
+				$flexThumbs.addClass('botiga-slides');
+				$flexThumbs.wrapAll('<div class="botiga-flexslider"></div>');
 
-          var $slider   = $gallery.find('.botiga-flexslider');
-        var itemWidth = ( $gallery.parent().is('.gallery-quickview') ) ? 85 : 95;
+				var $slider = $gallery.find('.botiga-flexslider');
+				var itemWidth = ($gallery.parent().is('.gallery-quickview')) ? 85 : 95;
 
-                $slider.flexslider({
-                  namespace: 'botiga-flex-',
-                  selector: '.botiga-slides > li',
-                  animation: 'slide',
-                  controlNav: false,
-                  animationLoop: false,
-                  slideshow: false,
-                  itemWidth: itemWidth,
-                  itemMargin: 20,
-                  keyboard: false,
-                  asNavFor: $gallery.get(0),
-                });
+				$slider.flexslider({
+					namespace: 'botiga-flex-',
+					selector: '.botiga-slides > li',
+					animation: 'slide',
+					controlNav: false,
+					animationLoop: false,
+					slideshow: false,
+					itemWidth: itemWidth,
+					itemMargin: 20,
+					keyboard: false,
+					asNavFor: $gallery.get(0),
+				});
 
-            }
+			}
 
-        });
+		});
 
-    },
+	},
 
 }
 
-jQuery( document ).ready(function(){
-    botiga.gallery.init();
+jQuery(document).ready(function () {
+	botiga.gallery.init();
 });
