34422a933c6acf9b284b4866a70a592093a786eb
Rodrigo Teixeira <maaprote@gmail.com>
2022-09-23 17:13:09 -0300

    identation

--- sidebar-shop.php
+++ sidebar-shop.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar for shop
  *
@@ -9,40 +10,40 @@
 
 $shop_sidebar_id = '';
 
-if ( is_shop() || is_product_category() || is_product_tag() || is_product_taxonomy() ) {
-  $shop_sidebar_id = get_theme_mod( 'shop_sidebar', 'shop-sidebar-1' );
-} else if ( is_singular( 'product' ) ) {
-  $shop_sidebar_opt  = get_theme_mod( 'shop_single_sidebar', 'shop-sidebar-1' );
-  $shop_sidebar_meta = get_post_meta( get_the_ID(), '_botiga_sidebar', true );
-  $shop_sidebar_id   = ( ! empty( $shop_sidebar_meta ) ) ? $shop_sidebar_meta : $shop_sidebar_opt;
+if (is_shop() || is_product_category() || is_product_tag() || is_product_taxonomy()) {
+	$shop_sidebar_id = get_theme_mod('shop_sidebar', 'shop-sidebar-1');
+} else if (is_singular('product')) {
+	$shop_sidebar_opt  = get_theme_mod('shop_single_sidebar', 'shop-sidebar-1');
+	$shop_sidebar_meta = get_post_meta(get_the_ID(), '_botiga_sidebar', true);
+	$shop_sidebar_id   = (!empty($shop_sidebar_meta)) ? $shop_sidebar_meta : $shop_sidebar_opt;
 }
 
-if ( empty( $shop_sidebar_id ) ) {
-  $shop_sidebar_id = 'shop-sidebar-1';
+if (empty($shop_sidebar_id)) {
+	$shop_sidebar_id = 'shop-sidebar-1';
 }
 
-$custom_sidebars = json_decode( get_theme_mod( 'custom_sidebars', '[]' ), true );
+$custom_sidebars = json_decode(get_theme_mod('custom_sidebars', '[]'), true);
 
-if ( ! empty( $custom_sidebars ) ) {
-  foreach ( $custom_sidebars as $custom_sidebar ) {
-    if ( ! empty( $custom_sidebar['conditions'] ) && botiga_get_display_conditions( $custom_sidebar['conditions'], false ) ) {
-      $shop_sidebar_id = sanitize_key( $custom_sidebar['name'] );
-    }
-  }
+if (!empty($custom_sidebars)) {
+	foreach ($custom_sidebars as $custom_sidebar) {
+		if (!empty($custom_sidebar['conditions']) && botiga_get_display_conditions($custom_sidebar['conditions'], false)) {
+			$shop_sidebar_id = sanitize_key($custom_sidebar['name']);
+		}
+	}
 }
 
-if ( ! is_active_sidebar( $shop_sidebar_id ) && ! is_active_sidebar( 'sidebar-1' ) ) {
+if (!is_active_sidebar($shop_sidebar_id) && !is_active_sidebar('sidebar-1')) {
 	return;
 }
 
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php do_action( 'botiga_before_sidebar' ); ?>
-	<?php if( is_active_sidebar( $shop_sidebar_id ) ) {
-		dynamic_sidebar( $shop_sidebar_id );
+	<?php do_action('botiga_before_sidebar'); ?>
+	<?php if (is_active_sidebar($shop_sidebar_id)) {
+		dynamic_sidebar($shop_sidebar_id);
 	} else {
-		dynamic_sidebar( 'sidebar-1' );
+		dynamic_sidebar('sidebar-1');
 	} ?>
-	<?php do_action( 'botiga_after_sidebar' ); ?>
+	<?php do_action('botiga_after_sidebar'); ?>
 </aside><!-- #secondary -->
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The sidebar containing the main widget area
  *
@@ -9,36 +10,36 @@
 
 $sidebar_id = '';
 
-if ( is_home() && ! is_front_page() ) {
-  $sidebar_id = get_theme_mod( 'blog_archive_sidebar', 'sidebar-1' );
-} else if ( is_singular( array( 'post', 'page' ) ) ) {
-  $sidebar_opt  = get_theme_mod( 'blog_single_sidebar', 'sidebar-1' );
-  $sidebar_meta = get_post_meta( get_the_ID(), '_botiga_sidebar', true );
-  $sidebar_id   = ( ! empty( $sidebar_meta ) ) ? $sidebar_meta : $sidebar_opt;
+if (is_home() && !is_front_page()) {
+	$sidebar_id = get_theme_mod('blog_archive_sidebar', 'sidebar-1');
+} else if (is_singular(array('post', 'page'))) {
+	$sidebar_opt  = get_theme_mod('blog_single_sidebar', 'sidebar-1');
+	$sidebar_meta = get_post_meta(get_the_ID(), '_botiga_sidebar', true);
+	$sidebar_id   = (!empty($sidebar_meta)) ? $sidebar_meta : $sidebar_opt;
 }
 
-if ( empty( $sidebar_id ) ) {
-  $sidebar_id = 'sidebar-1';
+if (empty($sidebar_id)) {
+	$sidebar_id = 'sidebar-1';
 }
 
-$custom_sidebars = json_decode( get_theme_mod( 'custom_sidebars', '[]' ), true );
+$custom_sidebars = json_decode(get_theme_mod('custom_sidebars', '[]'), true);
 
-if ( ! empty( $custom_sidebars ) ) {
-  foreach ( $custom_sidebars as $custom_sidebar ) {
-    if ( ! empty( $custom_sidebar['conditions'] ) && botiga_get_display_conditions( $custom_sidebar['conditions'], false ) ) {
-      $sidebar_id = sanitize_key( $custom_sidebar['name'] );
-    }
-  }
+if (!empty($custom_sidebars)) {
+	foreach ($custom_sidebars as $custom_sidebar) {
+		if (!empty($custom_sidebar['conditions']) && botiga_get_display_conditions($custom_sidebar['conditions'], false)) {
+			$sidebar_id = sanitize_key($custom_sidebar['name']);
+		}
+	}
 }
 
-if ( ! is_active_sidebar( $sidebar_id ) ) {
+if (!is_active_sidebar($sidebar_id)) {
 	return;
 }
 
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php do_action( 'botiga_before_sidebar' ); ?>
-	<?php dynamic_sidebar( $sidebar_id ); ?>
-	<?php do_action( 'botiga_after_sidebar' ); ?>
+	<?php do_action('botiga_before_sidebar'); ?>
+	<?php dynamic_sidebar($sidebar_id); ?>
+	<?php do_action('botiga_after_sidebar'); ?>
 </aside><!-- #secondary -->
