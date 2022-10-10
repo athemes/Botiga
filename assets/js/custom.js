8e0ae3deef54a29156087913b8bd696c57593703
Rodrigo Teixeira <maaprote@gmail.com>
2022-10-10 16:18:56 -0300

    Single Product Navigation: Mobile and Tablet issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,6 +29,10 @@ botiga.helpers = {
 
     document.addEventListener('DOMContentLoaded', fn, false);
   },
+  isInViewport: function isInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  },
   ajax: function ajax(action, nonce, extraParams, successCallback) {
     var ajax = new XMLHttpRequest();
     ajax.open('POST', botiga.ajaxurl, true);
@@ -334,13 +338,8 @@ botiga.navigation = {
       return false;
     }
 
-    if (isInViewport(submenu) == false && !submenu.closest('.menu-item').classList.contains('botiga-mega-menu')) {
+    if (botiga.helpers.isInViewport(submenu) == false && !submenu.closest('.menu-item').classList.contains('botiga-mega-menu')) {
       submenu.classList.add('sub-menu-reverse');
-    }
-
-    function isInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
     }
   }
 };
--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -19,6 +19,15 @@ botiga.helpers = {
 	
 		document.addEventListener( 'DOMContentLoaded', fn, false );
 	},
+	isInViewport: function( el ) {
+		const rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	},
 	ajax: function( action, nonce, extraParams, successCallback ) {
 		var ajax = new XMLHttpRequest();
 		ajax.open('POST', botiga.ajaxurl, true);
@@ -275,18 +284,8 @@ botiga.navigation = {
 			return false;
 		}
 
-		if( isInViewport( submenu ) == false && ! submenu.closest( '.menu-item' ).classList.contains( 'botiga-mega-menu' ) ) {
+		if( botiga.helpers.isInViewport( submenu ) == false && ! submenu.closest( '.menu-item' ).classList.contains( 'botiga-mega-menu' ) ) {
 			submenu.classList.add( 'sub-menu-reverse' );
-		}
-
-		function isInViewport(el) {
-			const rect = el.getBoundingClientRect();
-			return (
-				rect.top >= 0 &&
-				rect.left >= 0 &&
-				rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-				rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-			);
 		}
 	}
 
