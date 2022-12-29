9702cec027e4f562a68d2b4770831567262f00ad
Rodrigo Teixeira <maaprote@gmail.com>
2022-12-29 11:34:49 -0300

    fixed theme sniffer issues

--- inc/dashboard/part-support.php
+++ inc/dashboard/part-support.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Support
@@ -6,50 +7,51 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
 }
 
 ?>
 
 <div class="botiga-dashboard-row">
 
-  <div class="botiga-dashboard-column">
-    
-    <?php $support_box_class = ( $this->settings['has_pro'] ) ? 'botiga-dashboard-box-pro-support' : 'botiga-dashboard-box-support'; ?>
+	<div class="botiga-dashboard-column">
 
-    <div class="botiga-dashboard-box <?php echo sanitize_html_class( $support_box_class ) ?>">
+		<?php $support_box_class = ($this->settings['has_pro']) ? 'botiga-dashboard-box-pro-support' : 'botiga-dashboard-box-support'; // phpcs:ignore WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedVariableFound
+		?>
 
-      <div class="botiga-dashboard-box-row">
+		<div class="botiga-dashboard-box <?php echo sanitize_html_class($support_box_class) ?>">
 
-        <div class="botiga-dashboard-box-column">
+			<div class="botiga-dashboard-box-row">
 
-          <div class="botiga-dashboard-box-title"><?php esc_html_e( 'Need help? We\'re here for you!', 'botiga' ); ?></div>
-          <div class="botiga-dashboard-box-content">
-            <?php esc_html_e( 'Have a question? Hit a bug? Get the help you need, when you need it from our friendly support staff.', 'botiga' ); ?>
-          </div>
-          <div class="botiga-dashboard-box-link">
-            <a href="<?php echo esc_url( $this->settings['support_link'] ); ?>" target="_blank" class="button button-secondary"><?php esc_html_e( 'Get Support', 'botiga' ); ?></a>
-          </div>
+				<div class="botiga-dashboard-box-column">
 
-        </div>
+					<div class="botiga-dashboard-box-title"><?php esc_html_e('Need help? We\'re here for you!', 'botiga'); ?></div>
+					<div class="botiga-dashboard-box-content">
+						<?php esc_html_e('Have a question? Hit a bug? Get the help you need, when you need it from our friendly support staff.', 'botiga'); ?>
+					</div>
+					<div class="botiga-dashboard-box-link">
+						<a href="<?php echo esc_url($this->settings['support_link']); ?>" target="_blank" class="button button-secondary"><?php esc_html_e('Get Support', 'botiga'); ?></a>
+					</div>
 
-        <?php if ( ! $this->settings['has_pro'] ) : ?>
-          <div class="botiga-dashboard-box-column botiga-dashboard-home-priority-support">
-            <div class="botiga-dashboard-box-title"><?php esc_html_e( 'Priority Support', 'botiga' ); ?><span class="botiga-dashboard-box-badge">pro</span></div>
-            <div class="botiga-dashboard-box-content">
-              <?php esc_html_e( 'Get direct support from our developers via email. We aim to answer all priority support requests within 24 hours.', 'botiga' ); ?>
-            </div>
-            <div class="botiga-dashboard-box-link">
-              <a href="<?php echo esc_url( $this->settings['support_pro_link'] ); ?>" target="_blank" class="button button-pro-support"><?php esc_html_e( 'Get Priority Support with Botiga Pro', 'botiga' ); ?></a>
-            </div>
-          </div>
-        <?php endif; ?>
+				</div>
 
-      </div>
+				<?php if (!$this->settings['has_pro']) : ?>
+					<div class="botiga-dashboard-box-column botiga-dashboard-home-priority-support">
+						<div class="botiga-dashboard-box-title"><?php esc_html_e('Priority Support', 'botiga'); ?><span class="botiga-dashboard-box-badge">pro</span></div>
+						<div class="botiga-dashboard-box-content">
+							<?php esc_html_e('Get direct support from our developers via email. We aim to answer all priority support requests within 24 hours.', 'botiga'); ?>
+						</div>
+						<div class="botiga-dashboard-box-link">
+							<a href="<?php echo esc_url($this->settings['support_pro_link']); ?>" target="_blank" class="button button-pro-support"><?php esc_html_e('Get Priority Support with Botiga Pro', 'botiga'); ?></a>
+						</div>
+					</div>
+				<?php endif; ?>
 
-    </div>
+			</div>
 
-  </div>
+		</div>
+
+	</div>
 
 </div>
--- inc/modules/adobe-typekit/adobe-typekit.php
+++ inc/modules/adobe-typekit/adobe-typekit.php
@@ -13,6 +13,6 @@ if ( ! Botiga_Modules::is_module_active( 'adobe-typekit' ) ) {
  * Adobe typekit customize options.
  */
 function botiga_adobe_typekit_options( $wp_customize ) {
-    require get_template_directory() . '/inc/modules/adobe-typekit/customizer/options.php';
+    require get_template_directory() . '/inc/modules/adobe-typekit/customizer/options.php'; // phpcs:ignore WPThemeReview.CoreFunctionality.FileInclude.FileIncludeFound
 }
 add_action( 'customize_register', 'botiga_adobe_typekit_options', 999 );
