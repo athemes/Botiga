9fd6c6044bc48ad9804c9ee4d47d2967cefc8c87
Rodrigo Teixeira <maaprote@gmail.com>
2024-02-07 14:01:42 -0300

    accordion and extra checkout compatibility with woo subscriptions

---
+++ assets/js/botiga-accordion.js
@@ -0,0 +1,14 @@
+/**
+ * Botiga Accordion
+ * 
+ * jQuery Dependant: true
+ * 
+ */
+
+'use strict';
+
+var botiga = botiga || {};
+botiga.accordion = {};
+jQuery(document).ready(function () {
+  botiga.accordion.init();
+});
---
+++ assets/js/src/botiga-accordion.js
@@ -0,0 +1,18 @@
+/**
+ * Botiga Accordion
+ * 
+ * jQuery Dependant: true
+ * 
+ */
+
+'use strict';
+
+let botiga = botiga || {};
+
+botiga.accordion = {
+
+}
+
+jQuery(document).ready(function () {
+	botiga.accordion.init();
+});
