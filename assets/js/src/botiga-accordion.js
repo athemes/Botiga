372759b2bdee1e00e6bbb34ca812343e3acd58cc
Rodrigo Teixeira <maaprote@gmail.com>
2024-02-08 13:58:43 -0300

    add accordion style to filter widgets

--- assets/js/src/botiga-accordion.js
+++ assets/js/src/botiga-accordion.js
@@ -4,15 +4,51 @@
  * jQuery Dependant: true
  * 
  */
+(function($){
 
-'use strict';
+	'use strict';
 
-let botiga = botiga || {};
+	let botiga = botiga || {};
 
-botiga.accordion = {
+	botiga.accordion = {
 
-}
+		/**
+		 * Init
+		 * 
+		 * @return {void}
+		 */
+		init: function () {
+			const self = this;
 
-jQuery(document).ready(function () {
-	botiga.accordion.init();
-});
+			$( '.botiga-accordion' ).each( function() {
+				const toggle = $( this ).find( '.botiga-accordion-toggle' );
+				const content = $( this ).find( '.botiga-accordion-body' );
+
+				toggle.on( 'click', function() { self.slideToggleEffect( $( this ), content ); });
+				toggle.on( 'keyup', function(e) {
+					if ( e.keyCode === 13 ) {
+						self.slideToggleEffect( $( this ), content );
+					}
+				});
+			});
+		},
+		
+		/**
+		 * Slide Toggle Effect
+		 * 
+		 * @param {object} triggerEl
+		 * @param {object} content
+		 * 
+		 * @return {void}
+		 */
+		slideToggleEffect: function (triggerEl, content) {
+			content.slideToggle( 300 );
+			triggerEl.toggleClass( 'active' );
+		}
+	}
+
+	$(document).ready(function () {
+		botiga.accordion.init();
+	});
+
+})(jQuery);
