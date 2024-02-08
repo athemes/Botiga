4f9bb53ca022f81c7dc739d27348d96da9aa97ca
Rodrigo Teixeira <maaprote@gmail.com>
2024-02-08 14:20:02 -0300

    fix phpcs

--- inc/classes/class-botiga-custom-css.php
+++ inc/classes/class-botiga-custom-css.php
@@ -176,7 +176,7 @@ if ( !class_exists( 'Botiga_Custom_CSS' ) ) :
 						'h6', 
 						'.site-title', 
 						'.wc-block-grid__product-title',
-						'.checkout .recurring-totals > th'
+						'.checkout .recurring-totals > th',
 					);
 					$selector = self::get_mounted_selector( $selectors, $empty_selector );
 					
