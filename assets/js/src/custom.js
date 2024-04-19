3f100e81085859c77fd5f2ee568e4ab55dc8440e
Rodrigo Teixeira <maaprote@gmail.com>
2024-04-19 17:59:42 -0300

    vertical mega menu

--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -1505,30 +1505,74 @@ botiga.tabsNav = {
 	events: function() {
 		const tabsNavItems = document.querySelectorAll( '.botiga-tabs-nav-item' );
 		for( const tabItem of tabsNavItems ) {
+			const hasClickOnMouseOver = tabItem.closest( '.botiga-tabs-nav' ).classList.contains( 'botiga-tabs-nav-click-on-mouseover' );
+
+			if ( hasClickOnMouseOver ) {
+				let st;
+
+				tabItem.addEventListener( 'mouseover', function(e){
+					e.preventDefault();
+
+					const self = this;
+					
+					st = setTimeout(function(){
+						self.dispatchEvent( new Event( 'click' ) );
+					}, 500);
+				} );
+
+				tabItem.addEventListener( 'mouseout', function(e){
+					clearTimeout(st);
+				});
+			}
+
 			tabItem.addEventListener( 'click', function(e){
 				e.preventDefault();
 
 				const
-					tabId      = this.querySelector( '.botiga-tabs-nav-link' ).getAttribute( 'href' ),
-					tabContent = document.querySelector( tabId );
+					_this             = this,
+					tabId             = this.querySelector( '.botiga-tabs-nav-link' ).getAttribute( 'href' ),
+					previousTabId     = this.closest( '.botiga-tabs-nav' ).querySelector( '.botiga-tabs-nav-item.is-active .botiga-tabs-nav-link' ).getAttribute( 'href' ),
+					previousActiveTab = document.querySelector( previousTabId ),
+					tabContentTo      = document.querySelector( tabId ),
+					activeTabsCount   = tabContentTo.parentNode.querySelectorAll( '.botiga-tab-content.is-active' ).length;
 
-				if ( tabContent === null ) {
+				if ( tabContentTo === null ) {
+					return false;
+				}
+
+				if( activeTabsCount === 0 || activeTabsCount > 1 ) {
+					return false;
+				}
+				
+				if ( tabContentTo.classList.contains( 'is-active' ) ) {
 					return false;
 				}
 
 				for( const tabItem of tabsNavItems ) {
-					const tabContentId = document.querySelector( tabItem.querySelector( '.botiga-tabs-nav-link' ).getAttribute( 'href' ) );
+					const tabContentTo = document.querySelector( tabItem.querySelector( '.botiga-tabs-nav-link' ).getAttribute( 'href' ) );
 					
-					if ( tabContentId === null ) {
+					if ( tabContentTo === null ) {
 						continue;
 					}
 
 					tabItem.classList.remove( 'is-active' );
-					document.querySelector( tabItem.querySelector( '.botiga-tabs-nav-link' ).getAttribute( 'href' ) ).classList.remove( 'is-active' );
+					tabContentTo.classList.remove( 'is-active' );
 				}
 
-				this.classList.add( 'is-active' );
-				tabContent.classList.add( 'is-active' );
+				previousActiveTab.classList.add( 'removing' );
+
+				setTimeout(function(){
+					_this.classList.add( 'is-active' );
+
+					tabContentTo.classList.add( 'activating' );
+					setTimeout(function(){
+						tabContentTo.classList.add( 'is-active' );
+
+						tabContentTo.classList.remove( 'activating' );
+					}, 300);
+					
+					previousActiveTab.classList.remove( 'removing' );
+				}, 300);
 
 			} );
 		}
