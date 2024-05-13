e013e26decc1534c9e06b097557075e32ef1a021
Rodrigo Teixeira <maaprote@gmail.com>
2024-05-13 14:34:53 -0300

    update support copy. Change thank you message link

--- inc/dashboard/html-home.php
+++ inc/dashboard/html-home.php
@@ -204,7 +204,7 @@ if (!defined('ABSPATH')) {
                         <h2><?php echo esc_html__( 'Premium support', 'botiga' ); ?></h2>
                     </div>
                     <div class="botiga-dashboard-card-body">
-                        <p><?php echo esc_html__( 'Get direct support from our developers via email. We aim to answer all premium support requests within 24 hours.', 'botiga' ); ?></p>
+                        <p><?php echo esc_html__( 'Get support from our team of WordPress experts via email. We aim to respond within one business day, but we often manage to reply much sooner.', 'botiga' ); ?></p>
                         <a href="<?php echo esc_url( $this->settings['upgrade_pro'] ); ?>" class="botiga-dashboard-external-link" target="_blank">
                             <?php echo esc_html__( 'Get Premium Support With Botiga Pro', 'botiga' ); ?>
                             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
