9279510b3d75ee9c927e43965db7d1ebc5f09c30
Rodrigo Teixeira <maaprote@gmail.com>
2024-05-17 11:49:19 -0300

    Add template overriding comments to template files

--- template-parts/search/content-ajax-search-see-all-button.php
+++ template-parts/search/content-ajax-search-see-all-button.php
@@ -1,8 +1,16 @@
 <?php
 /**
- * Template part for displaying ajax search products loop end.
+ * Template part for displaying ajax search see all button.
+ * 
+ * This template can be overridden by copying it to yourtheme/template-parts/search/content-ajax-search-see-all-button.php.
  *
- * @package Botiga
+ * HOWEVER, on occasion Botiga will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen.
+ *
+ * @package Botiga\Templates
+ * @version 2.2.4
  */
 
 // Exit if accessed directly.
--- template-parts/search/content-ajax-search.php
+++ template-parts/search/content-ajax-search.php
@@ -1,8 +1,16 @@
 <?php
 /**
  * Template part for displaying ajax search content.
+ * 
+ * This template can be overridden by copying it to yourtheme/template-parts/search/content-ajax-search.php.
  *
- * @package Botiga
+ * HOWEVER, on occasion Botiga will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen.
+ *
+ * @package Botiga\Templates
+ * @version 2.2.4
  */
 
 // Exit if accessed directly.
