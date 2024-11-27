96dee85a4965c563f1ce4cbe5f6600d6b7debfaf
Rodrigo Teixeira <maaprote@gmail.com>
2024-11-27 11:35:34 -0300

    Fix mobile menu categories display class being added without the feature enabled

--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -135,14 +135,11 @@ botiga.navigation = {
 
 			document.body.classList.add( 'mobile-menu-visible' )
 			
-			//Toggle submenus
+			// Toggle submenus
 			var submenuToggles = offCanvas.querySelectorAll( '.dropdown-symbol, .menu-item-has-children > a[href="#"]' );
 			for ( var submenuToggle of submenuToggles ) {
-				if ( 'ontouchstart' in window ) {
-					submenuToggle.addEventListener( 'touchstart', submenuToggleHandler );
-				} else {
-					submenuToggle.addEventListener( 'click', submenuToggleHandler );
-				}
+				submenuToggle.addEventListener( 'touchstart', submenuToggleHandler );
+				submenuToggle.addEventListener( 'click', submenuToggleHandler );
 
 				submenuToggle.addEventListener('keydown', function(e) {
 					var isTabPressed = (e.key === 'Enter' || e.keyCode === 13);
@@ -161,10 +158,22 @@ botiga.navigation = {
 		} );
 
 		function submenuToggleHandler(e) {
-			e.preventDefault();
+			if ( e.cancelable ) {
+				e.preventDefault();
+			}
 
 			var parent = e.target.closest( 'li' );
+
+			if ( parent.querySelector( '.sub-menu' ).classList.contains( 'toggling' ) ) {
+				return false;
+			}
+
+			parent.querySelector( '.sub-menu' ).classList.toggle( 'toggling' );
 			parent.querySelector( '.sub-menu' ).classList.toggle( 'toggled' );
+
+			setTimeout(function(){
+				parent.querySelector( '.sub-menu' ).classList.remove( 'toggling' );
+			}, 300);
 		}
 
 		// Close the offcanvas when a anchor that contains a hash is clicked
